805f3f25a7e4fc2a26329750fd2b599590746f9c
Sriram Madhivanan <sriram.inuyasha@gmail.com>
2016-03-28 01:15:22 -0400

    used function pointers

--- testMulTreads.c
+++ testMulTreads.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
- 
+
+#define smunch(arg,sig) syscall(329,arg,sig) 
 #define NUM_THREADS 2
  
 /* create thread argument struct for thr_func() */
@@ -13,22 +14,31 @@ typedef struct _thread_data_t {
 /* thread function */
 void *thr_func(void *arg) {
   thread_data_t *data = (thread_data_t *)arg;
- 
+ smunch(getpid(), 10);
   printf("hello from thr_func, thread id: %d\n", data->tid);
  
   pthread_exit(NULL);
 }
- 
+
+void *second(void * arg){
+  thread_data_t *data = (thread_data_t *)arg;
+  printf("I do not execute smunch \n");
+  pthread_exit(NULL);
+}
+
+void *(*func[2])(void *arg);
 int main(int argc, char **argv) {
   pthread_t thr[NUM_THREADS];
   int i, rc;
+  func[0] = thr_func;
+  func[1] = second;
   /* create a thread_data_t argument array */
   thread_data_t thr_data[NUM_THREADS];
  
   /* create threads */
   for (i = 0; i < NUM_THREADS; ++i) {
     thr_data[i].tid = i;
-    if ((rc = pthread_create(&thr[i], NULL, thr_func, &thr_data[i]))) {
+    if ((rc = pthread_create(&thr[i], NULL, func[i], &thr_data[i]))) {
       fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
       return EXIT_FAILURE;
     }
